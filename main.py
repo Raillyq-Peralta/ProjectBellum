236bb7eef55cef17289ddab9d18ae40410124932
Raillyq Peralta <rsperalta@carc.pshs.edu.ph>
2026-03-05 22:30:33 +0800

    Added typing effect, input detection, and more JSONS ( V1.0 )

--- main.py
+++ main.py
@@ -1,31 +1,58 @@
 # ========== LIBRARIES ========== #
 import json
 import time
+import sys
 import random
+try:
+    import keyboard
+except ImportError:
+    print("Error: Module 'keyboard' not found. Please install it and try again.")
+    quit()
+
+# ========== JSON INTERPRETING ========== #
+with open("player_data.json", "r") as file:
+    player_data = json.load(file)
+
+with open("information.json", "r") as file:
+    information_data = json.load(file)
+
+with open("dialogues.json", "r") as file:
+    dialogues = json.load(file)
+
+with open("inventory.json", "r") as file:
+    inventory = json.load(file)
 
 # ========== FUNCTIONS ========== #
+
+# ===== INPUT DETECTION ===== #
+def press_key():
+    print(" ➤ ", end="")
+    keyboard.read_event(suppress=True)
+
+# ===== TYPING EFFECT ===== #
+def type_text(text):
+    for char in text:
+        print(char, end="", flush=True)
+        time.sleep(0.05)
+
+
+# ===== MAIN GAME ===== #
 def game():
-    time.sleep(1)
-    print("Welcome to Project Bellum")
-    time.sleep(1.5)
-    print("You're a single middle aged man who works a regular 9-5 office job and its been a long day at work.")
-    time.sleep(4.5)
-    print("It's time to go home, you decide to take the bus home and you fall asleep because of how tired you are.")
-
-
-
-
-
-
-
-
-
-def tutorial():
+    # ===== INTRO ===== #
     print("⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚")
-    print("                                   HOW TO PLAY                                      ")
-    print("⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚")
-    print("                                                                                    ")
-    print("")
+    for line in dialogues["intro"]:
+        type_text(line)
+        print()
+        press_key()
+
+
+def information():
+    print("=============== INFORMATION ===============")
+    for line in information_data:
+        print(line)
+    press_key()
+    print()
+
 
 # ========== MAIN GAME LOOP ========== #
 while True:
@@ -37,23 +64,23 @@ while True:
 
         # ========== MAIN MENU ========== #
         while True:
-            print("▣▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▣")
-            print("▥                                 PROJECT BELLUM                                    ▥")
-            print("▣▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▣")
+            print("▣====================================================================================▣")
+            print("▥                                 PROJECT BELLUM                                     ▥")
+            print("▣====================================================================================▣")
             print("                                                                                       ")
-            print("                          ▣▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▣                              ")
+            print("                          ▣============================▣                              ")
             print("                          ▥            PLAY            ▥                              ")
-            print("                          ▣▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▣                              ")
+            print("                          ▣============================▣                              ")
             print("                                        [1]                                            ")
             print("                                                                                       ")
-            print("                          ▣▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▣                              ")
-            print("                          ▥         HOW TO PLAY        ▥                              ")
-            print("                          ▣▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▣                              ")
+            print("                          ▣============================▣                              ")
+            print("                          ▥         INFORMATION        ▥                              ")
+            print("                          ▣============================▣                              ")
             print("                                        [2]                                            ")
             print("                                                                                       ")
-            print("                          ▣▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▣                              ")
+            print("                          ▣============================▣                              ")
             print("                          ▥            QUIT            ▥                              ")
-            print("                          ▣▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▣                              ")
+            print("                          ▣============================▣                              ")
             print("                                        [3]                                            ")
             print("                                                                                       ")
 
@@ -63,16 +90,20 @@ while True:
             if choice == "1":
                 game()
             elif choice == "2":
-                tutorial()
+                information()
             elif choice == "3":
                 quit()
             else:
                 print("⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚")
-                print("INVALID INPUT INVALID INPUT INVALID INPUT INVALID INPUT INVALID INPUT INVALID INPUT")
+                print("INVALID  INPUT INVALID INPUT INVALID INPUT  INVALID INPUT INVALID INPUT INVALID  INPUT")
                 print("⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚⬚")
                 print()
 
+
+
     except FileNotFoundError:
-        print("Error: The file 'data.json' was not found.")
+        print("Error: Some JSON files are missing, please reinstall and try again.")
+        quit()
     except json.JSONDecodeError as e:
-        print(f"Failed to decode JSON: {e}")
+        print(f"Error: Failed to decode JSON: {e}")
+        quit()
